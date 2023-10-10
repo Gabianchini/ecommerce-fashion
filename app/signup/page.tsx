847b52578990a5836421f191768aeb0e8fd8b861
Gabianchini <gabibisdx@gmail.com>
2023-10-10 00:12:40 -0300

    ✨Add feature Signup

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignForm from "./signForm";
+
+type Props = {}
+
+const Page = (props:Props) => {
+    return(
+        <SignForm/>
+    )
+}
+
+export default Page
