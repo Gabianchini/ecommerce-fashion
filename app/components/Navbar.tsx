f70a6780fc1845f67b2884317224fdb1a9d23da7
Gabianchini <gabibisdx@gmail.com>
2023-10-13 19:57:41 -0300

    ✨ Add feature  user authentication

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,12 +5,35 @@ import {CiShoppingCart} from "react-icons/ci"
 import {BsChevronCompactUp} from "react-icons/bs"
 import {BiSearch} from "react-icons/bi"
 import SearchBar from "./SearchBar";
+import {signOut, signIn, useSession} from 'next-auth/react'
 
 type Props = {}
 
 const Navbar = (props:Props) => {
     const [showProfile, setShowProfile] =useState<boolean>(false)
     const [showNav, setShowNav] = useState<boolean>(false)
+    const {data:session} = useSession()
+    console.log(session?.user)
+
+    const SignOut = () => {
+        if(session && session.user){
+            return(
+                <ul className="py-5 px-1 text-neutral-600">
+                    <li className="hover:bg-gray-100 hover:text-neutral-900 px-5 py-2 cursor-pointer">
+                        {session.user.name}
+                    </li>
+                    <li onClick={() => signOut()} className="whitespace-nowrap hover:text-red-600 px-5 py-2 cursor-pointer">SignOut</li>
+                    <li className="whitespace-nowrap hover:bg-gray-100 hover:text-neutral-900 px-5 py-2 cursor-pointer"><a href="/addproduct">Add Product</a></li>
+                </ul>
+            )
+        }
+        return(
+            <ul>
+                <li onClick={()=>signIn()} className="whitespace-nowrap hover:bg-gray-100 hover:text-neutral-900 px-5 py-2 cursor-pointer"></li>
+            </ul>
+        )
+    }
+
     return(
         <div>
             <div className="flex items-center justify-between py-4 relative">
@@ -20,7 +43,8 @@ const Navbar = (props:Props) => {
                         <ul className="flex items-center lg:space-x-10 space-x-7 opacity-70 text-[15px]">
                             <li><a href="/" className="py-3 inline-block w-full">Shop</a></li>
                             <li><a href="filters" className="py-3 inline-block w-full">Filters</a></li>
-                            <li><a href="myproducts" className="py-3 inline-block w-full">My products</a></li>
+                            {session?.user && (
+                            <li><a href="myproducts" className="py-3 inline-block w-full">My products</a></li>)}
                         </ul>
                     </nav>
                 </div>    
@@ -29,7 +53,7 @@ const Navbar = (props:Props) => {
                     <div onClick={() => setShowProfile(!showProfile)} className="relative cursor-pointer">
                             <img src="user.jpg" className="w-[35px] h-[35px] rounded-full object-cover" alt=""/>
                             <div className={`absolute bg-white z-[2] rounded-lg shadow-lg ${showProfile ? "":"hidden"}`}>
-                                <Link href='/sign'>SignIn</Link>
+                                <SignOut/>
                             </div>     
                     </div>
                             <Link href='/cart'>
---
+++ app/signin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SigninForm from './signinform'
+
+type Props = {}
+
+const page = (props:Props) => {
+    return(
+        <SigninForm/>
+    )
+}
+
+export default page
