a13cf5bcf56191eee60f005de08a6ddb58c9ee7d
Gabianchini <gabibisdx@gmail.com>
2023-10-17 17:22:50 -0300

    ✨ Add feature product Form

---
+++ app/addproduct/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from "react";
+import Productform from "./productform";
+
+type Props = {}
+
+const page = (props: Props) => {
+    return(
+    <Productform/>
+    )
+}
+
+export default page
