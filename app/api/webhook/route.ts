9a57a437f3fd94ab606d5475695f9df7377261b9
Gabianchini <gabibisdx@gmail.com>
2024-01-02 16:24:09 -0300

    fix config

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,7 +3,7 @@ import prisma from "@/app/prismadb"
 import {NextResponse} from "next/server"
 import {headers} from "next/headers"
 
-const stripe =new Stripe(process.env.STRIPE_SECRET_KEY!, {apiVersion:'2023-08-16',typescript:true})
+const stripe =new Stripe(process.env.STRIPE_SECRET_KEY!, {typescript:true})
 
 export async function POST (req:Request){
     const body = await req.text()
